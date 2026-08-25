30f725b1e706c63600fac55b224225c0925a809b
catalinaIlabaca <catalina.a.ilabaca@uchile.cl>
2026-08-25 18:22:44 -0400

    feat: add common.py

--- eol_forum_notifications/settings/common.py
+++
@@ -1,2 +0,0 @@
-def plugin_settings(settings):
-    settings.EOL_FORUMS_NOTIFICATIONS_ENABLE = True
--- eoldiscussion/settings/common.py
+++ eoldiscussion/settings/common.py
@@ -1,2 +1,3 @@
 def plugin_settings(settings):
     settings.EOLGRADEDISCUSSION_LIMIT_THREAD = 5000
+    settings.EOL_FORUMS_NOTIFICATIONS_ENABLE = True
