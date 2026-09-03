eol-uchile/eol_xblock_discussion

chore!: rename app and bump version to 2.0.0

setup.py
@@ -23,8 +23,8 @@ def package_data(pkg, roots):
 
 
 setup(
-    name='eoldiscussion-xblock',
-    version='1.3.1',
+    name='eoldiscussion',
+    version='2.0.0',
     description='EOL Discussion Xblock',
     author="Oficina EOL UChile",
     author_email="eol-ing@uchile.cl",
@@ -36,6 +36,11 @@ setup(
         'xblock.v1': [
             'eoldiscussion = eoldiscussion:EolDiscussionXBlock',
         ],
-    },
-    package_data = {"eoldiscussion": ["static/**/*"]}
+        "lms.djangoapp": [
+            "eoldiscussion = eoldiscussion.apps:ImgAnnotationConfig",
+        ],
+        "cms.djangoapp": [
+            "eoldiscussion = eoldiscussion.apps:ImgAnnotationConfig",
+        ],
+    }
 )

eol-uchile/eol-discussion

setup.py
@@ -1,30 +1,11 @@
 #!/usr/bin/env python
 """ Setup to allow pip installs of eol discussion xblock """
 
-import os
-
 from setuptools import setup, find_packages
 
-
-def package_data(pkg, roots):
-    """Generic function to find package_data.
-
-    All of the files under each of the `roots` will be declared as package
-    data for package `pkg`.
-
-    """
-    data = []
-    for root in roots:
-        for dirname, _, files in os.walk(os.path.join(pkg, root)):
-            for fname in files:
-                data.append(os.path.relpath(os.path.join(dirname, fname), pkg))
-
-    return {pkg: data}
-
-
 setup(
-    name='eoldiscussion-xblock',
-    version='1.3.1',
+    name='eoldiscussion',
+    version='2.0.0',
     description='EOL Discussion Xblock',
     author="Oficina EOL UChile",
     author_email="eol-ing@uchile.cl",
@@ -36,6 +17,11 @@ setup(
         'xblock.v1': [
             'eoldiscussion = eoldiscussion:EolDiscussionXBlock',
         ],
-    },
-    package_data = {"eoldiscussion": ["static/**/*"]}
+        "lms.djangoapp": [
+            "eoldiscussion = eoldiscussion.apps:EolDiscussionConfig",
+        ],
+        "cms.djangoapp": [
+            "eoldiscussion = eoldiscussion.apps:EolDiscussionConfig",
+        ],
+    }
 )